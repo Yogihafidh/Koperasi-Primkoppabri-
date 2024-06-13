cacd47205263e3d1a8e7176c749a1d6edbbfbf4c
Yogihafidh <hafidhyogi@gmail.com>
2024-06-13 23:16:46 +0700

    edit link css

--- template/beranda.php
+++ template/beranda.php
@@ -7,10 +7,10 @@
     <title>Beranda</title>
 
     <!-- CSS -->
-    <link rel="stylesheet" href="../assets/css/general.css">
-    <link rel="stylesheet" href="../assets/css/style.css">
-    <link rel="stylesheet" href="../assets/css/query.css">
-    <link rel="stylesheet" href="../assets/css/helper.css">
+    <link rel="stylesheet" href="assets/css/general.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/query.css">
+    <link rel="stylesheet" href="assets/css/helper.css">
 </head>
 
 <body>
--- template/transaksi.php
+++ template/transaksi.php
@@ -6,10 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Transaksi</title>
     <!-- CSS -->
-    <link rel="stylesheet" href="../assets/css/general.css">
-    <link rel="stylesheet" href="../assets/css/style.css">
-    <link rel="stylesheet" href="../assets/css/query.css">
-    <link rel="stylesheet" href="../assets/css/helper.css">
+    <link rel="stylesheet" href="assets/css/general.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/query.css">
+    <link rel="stylesheet" href="assets/css/helper.css">
 
     <!-- JAVASCRIPT -->
     <script defer src="../assets/javascript/transaksi.js"></script>
